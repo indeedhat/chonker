7ebe4b5ebf805a9ca4f997a04faf2eed88384378
Matt Holmes <iam4423@gmail.com>
2025-12-05 22:01:12 +0000

    feat: send feed notification/errors via pushbullet

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/indeedhat/chonker
 go 1.25.4
 
 require (
+	github.com/indeedhat/dotenv v0.0.0-20250530135927-23ddeda9168e
 	github.com/indeedhat/icl v0.0.0-20241201163654-3fd7f368648f
-	github.com/indeedhat/malcolm v0.0.0-20251017132135-b5b615df8282
 )
+
+require github.com/stretchr/testify v1.11.1 // indirect
--- internal/report/report.go
+++ internal/report/report.go
@@ -1,5 +1,60 @@
 package report
 
-func ReportOnFeed(err error) {
-	panic("not implemented")
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+
+	"github.com/indeedhat/dotenv"
+)
+
+const (
+	pushEndpoint = "https://api.pushbullet.com/v2/pushes"
+)
+
+var (
+	recipientEmail dotenv.String = "PB_EMAIL"
+	authToken      dotenv.String = "PB_AUTH"
+)
+
+type Report interface {
+	Error() error
+	Message() string
+	Title() string
+}
+
+type pushMessage struct {
+	Type  string `json:"type"`
+	Email string `json:"email"`
+	Title string `json:"title"`
+	Body  string `json:"body"`
+}
+
+func ReportOnFeed(r Report) {
+	msg := pushMessage{
+		Type:  "note",
+		Email: recipientEmail.Get(),
+	}
+
+	if r.Error() != nil {
+		msg.Title = "Chonker Error"
+		msg.Body = r.Error().Error()
+	} else {
+		msg.Title = r.Title()
+		msg.Body = r.Message()
+	}
+
+	data, err := json.Marshal(msg)
+	if err != nil {
+		return
+	}
+
+	body := bytes.NewReader(data)
+	req, err := http.NewRequest("POST", pushEndpoint, body)
+	if err != nil {
+		return
+	}
+
+	req.Header.Add("content-type", "application/json")
+	req.Header.Add("Access-Token", authToken.Get())
 }
