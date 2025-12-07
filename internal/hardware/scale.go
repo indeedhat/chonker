5f0998f80babb344e7e2f1378d5cd5c7315569f0
Matt Holmes <iam4423@gmail.com>
2025-12-07 14:00:16 +0000

    refactor: scales working and tested

--- internal/hardware/scale.go
+++ internal/hardware/scale.go
@@ -2,42 +2,139 @@ package hardware
 
 import (
 	"context"
+	"sort"
+	"sync"
+	"time"
 
+	"github.com/indeedhat/dotenv"
 	"periph.io/x/devices/v3/hx711"
 	"periph.io/x/host/v3/rpi"
 )
 
-type scale struct {
-	hx *hx711.Dev
+const readTimeout = time.Millisecond * 500
+
+var (
+	adjustToGrams dotenv.Int = "ADJUST_TO_G"
+)
+
+type ringBuffer struct {
+	data []int
+	i    int
+	full bool
+	mux  sync.Mutex
 }
 
-func newScale() (*scale, error) {
+func newRingBuffer() *ringBuffer {
+	return &ringBuffer{data: make([]int, 11)}
+}
+
+func (b *ringBuffer) Set(v int32) {
+	b.mux.Lock()
+	defer b.mux.Unlock()
+
+	b.i++
+
+	if b.i == 10 {
+		b.full = true
+	}
+
+	i := b.i % 10
+	b.data[i] = int(v)
+}
+
+func (b *ringBuffer) Data() []int {
+	b.mux.Lock()
+	defer b.mux.Unlock()
+
+	out := make([]int, 11)
+	copy(out, b.data)
+
+	return out
+}
+
+type scale struct {
+	hx     *hx711.Dev
+	buf    *ringBuffer
+	cancel context.CancelFunc
+	Zero   float64
+	Scale  float64
+}
+
+func NewScale() (*scale, error) {
 	hx, err := hx711.New(
-		rpi.P1_5,
-		rpi.P1_3,
+		rpi.P1_29,
+		rpi.P1_31,
 	)
 	if err != nil {
 		return nil, err
 	}
 
-	return &scale{hx}, nil
-}
+	ctx, cancel := context.WithCancel(context.Background())
 
-func (s *scale) Await(ctx context.Context, weight float64) <-chan struct{} {
-	reading := s.hx.ReadContinuous()
-	var done chan struct{}
+	scale := &scale{
+		hx:     hx,
+		buf:    newRingBuffer(),
+		cancel: cancel,
+		Scale:  float64(adjustToGrams.Get(1)),
+	}
 
 	go func() {
-		defer s.Close()
+		readings := scale.hx.ReadContinuous()
+
 		for {
 			select {
 			case <-ctx.Done():
 				return
-			case r := <-reading:
-				// TODO: get the proper weight
-				w := float64(r.Raw)
+			case r := <-readings:
+				if r.Raw == -1 {
+					continue
+				}
+				scale.buf.Set(r.Raw)
+			}
+		}
+	}()
+
+	return scale, nil
+}
+
+func (s *scale) AutoZero() error {
+	_, _ = s.Weigh()
+
+	adjustScale := s.Scale
+	s.Scale = 1
+	s.Zero = 0
+
+	time.Sleep(time.Second * 2)
+
+	w, err := s.Weigh()
+	if err != nil {
+		return err
+	}
+
+	s.Scale = adjustScale
+	s.Zero = w
+
+	return nil
+}
+
+func (s *scale) Await(ctx context.Context, weight float64) <-chan struct{} {
+	var done chan struct{}
+
+	go func() {
+		ticker := time.NewTicker(readTimeout)
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case <-ticker.C:
+				if !s.buf.full {
+					continue
+				}
+
+				w, _ := s.Weigh()
 				if w >= weight {
 					done <- struct{}{}
+					ticker.Stop()
 					return
 				}
 			}
@@ -48,15 +145,18 @@ func (s *scale) Await(ctx context.Context, weight float64) <-chan struct{} {
 }
 
 func (s *scale) Weigh() (float64, error) {
-	r, err := s.hx.Read()
-	if err != nil {
-		return 0, err
+	for !s.buf.full {
+		time.Sleep(time.Millisecond * 100)
 	}
 
-	// TODO: find out what i need to do to convert this to g
-	return float64(r.Raw), nil
+	data := s.buf.Data()
+
+	sort.Ints(data)
+
+	return (float64(data[len(data)/2]) - s.Zero) / s.Scale, nil
 }
 
 func (s *scale) Close() {
 	s.hx.Halt()
+	s.cancel()
 }
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -9,6 +10,7 @@ import (
 	"syscall"
 	"time"
 
+	"github.com/indeedhat/chonker/internal/hardware"
 	"github.com/indeedhat/chonker/internal/schedule"
 	"github.com/indeedhat/chonker/internal/types"
 )
@@ -54,4 +56,66 @@ func main() {
 	if err := svr.Shutdown(shutdownCtx); err != nil {
 		log.Print("Server forced to shutdown after timeout")
 	}
+}
+
+func calibrateScale(weight1, weight2 float64) {
+	var (
+		adjustZero float64
+		reading1   float64
+		reading2   float64
+	)
+
+	scale, err := hardware.NewScale()
+	if err != nil {
+		log.Fatalf("failed to init scale: %s", err)
+	}
+
+	// NB: the adjustment values is pulled from the env so we need to make sure to reset it back to 1
+	//     to get accurate calibration values
+	scale.Scale = 1
+
+	fmt.Println("Make sure scale is working and empty, getting weight in 5 seconds...")
+	time.Sleep(5 * time.Second)
+
+	fmt.Println("Getting weight...")
+	adjustZero, err = scale.Weigh()
+	if err != nil {
+		fmt.Println("ReadDataMedianRaw error:", err)
+		return
+	}
+
+	fmt.Println("Raw weight is:", adjustZero)
+	fmt.Println("")
+
+	fmt.Printf("Put first weight of %.2f on scale, getting weight in 15 seconds...\n", weight1)
+	time.Sleep(15 * time.Second)
+
+	fmt.Println("Getting weight...")
+	reading1, err = scale.Weigh()
+	if err != nil {
+		fmt.Println("ReadDataMedianRaw error:", err)
+		return
+	}
+
+	fmt.Println("Raw weight is:", reading1)
+	fmt.Println("")
+
+	fmt.Printf("Put second weight of %.2f on scale, getting weight in 15 seconds...\n", weight2)
+	time.Sleep(15 * time.Second)
+
+	fmt.Println("Getting weight...")
+	reading2, err = scale.Weigh()
+	if err != nil {
+		fmt.Println("ReadDataMedianRaw error:", err)
+		return
+	}
+	fmt.Println("Raw weight is ", reading2)
+	fmt.Println("")
+
+	adjust1 := float64(reading1-adjustZero) / weight1
+	adjust2 := float64(reading2-adjustZero) / weight2
+
+	fmt.Println("AdjustZero should be set to:", adjustZero)
+	fmt.Printf("AdjustScale should be set to a value between %f and %f\n", adjust1, adjust2)
+	fmt.Println("")
 }
